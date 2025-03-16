df1f38584889ab3f61f54c2e011b48fbc18c3806
Rimngz <roarming0831@gmail.com>
2025-03-16 15:47:38 +0800

    changed README added Swap but has problems

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -45,9 +45,9 @@ public class Piece : MonoBehaviour
         lockTime += Time.deltaTime;
 
         // Handle rotation
-        if (Input.GetKeyDown(KeyCode.Q)) {
+        if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.Z)) {
             Rotate(-1);
-        } else if (Input.GetKeyDown(KeyCode.E)) {
+        } else if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.X)) {
             Rotate(1);
         }
 
@@ -73,18 +73,18 @@ public class Piece : MonoBehaviour
     private void HandleMoveInputs()
     {
         // Soft drop movement
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
-            if (Move(Vector2Int.down)) {
+            if (Move(Vector2Int.down) || Input.GetKeyDown(KeyCode.UpArrow)) {
                 // Update the step time to prevent double movement
                 stepTime = Time.time + stepDelay;
             }
         }
 
         // Left/right movement
-        if (Input.GetKey(KeyCode.A)) {
+        if (Input.GetKey(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) {
             Move(Vector2Int.left);
-        } else if (Input.GetKey(KeyCode.D)) {
+        } else if (Input.GetKey(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) {
             Move(Vector2Int.right);
         }
     }
