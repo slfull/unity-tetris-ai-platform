24b565d8de733c6dbeb9abb929ea7fe79e430b03
Rimngz <roarming0831@gmail.com>
2025-04-28 20:06:19 +0800

    fixed I piece bug, added title and buttons.

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -151,7 +151,7 @@ public class Piece : NetworkBehaviour
         board.SpawnPiece();
     }
 
-    public bool Move(Vector2Int translation)
+    private bool Move(Vector2Int translation)
     {
         Vector3Int newPosition = position;
         newPosition.x += translation.x;
@@ -170,7 +170,7 @@ public class Piece : NetworkBehaviour
         return valid;
     }
 
-    public void Rotate(int direction)
+    private void Rotate(int direction)
     {
         // Store the current rotation in case the rotation fails
         // and we need to revert
@@ -265,5 +265,40 @@ public class Piece : NetworkBehaviour
         }
     }
 
-   
+    public void RotateAgent(int direction)
+    {
+        // Store the current rotation in case the rotation fails
+        // and we need to revert
+        int originalRotation = rotationIndex;
+
+        // Rotate all of the cells using a rotation matrix
+        rotationIndex = Wrap(rotationIndex + direction, 0, 4);
+        ApplyRotationMatrix(direction);
+
+        // Revert the rotation if the wall kick tests fail
+        if (!TestWallKicks(rotationIndex, direction))
+        {
+            rotationIndex = originalRotation;
+            ApplyRotationMatrix(-direction);
+        }
+    }
+
+    public bool MoveAgent(Vector2Int translation)
+    {
+        Vector3Int newPosition = position;
+        newPosition.x += translation.x;
+        newPosition.y += translation.y;
+
+        bool valid = board.IsValidPosition(this, newPosition);
+
+        // Only save the movement if the new position is valid
+        if (valid)
+        {
+            position = newPosition;
+            moveTime = Time.time + moveDelay;
+            lockTime = 0f; // reset
+        }
+
+        return valid;
+    }
 }
