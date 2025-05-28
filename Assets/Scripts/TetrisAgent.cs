9833b9ed7f35a2e6fa3f55f16c9fcdd6323f8328
Rmingz <108404579+Rmingz@users.noreply.github.com>
2025-05-28 16:59:31 +0800

    ml-agent is working. TODO: tile detection.

--- Assets/Scripts/TetrisAgent.cs
+++ Assets/Scripts/TetrisAgent.cs
@@ -10,10 +10,17 @@ using Unity.MLAgents.Actuators;
 public class TetrisAgent : Agent
 {
     private Board board;
+    private Piece activePiece;
     public float timeBetweenDecisionsAtInference;
     float m_TimeSinceDecision;
+
+    public enum MovementInput
+    {
+        left, right, softdrop, harddrop, rotateclockwise, rotatecounterclockwise, hold
+    }
     public void FixedUpdate()
     {
+        activePiece = board.activePiece;
         WaitTimeInference();
     }
 
@@ -43,46 +50,56 @@ public class TetrisAgent : Agent
     {
         // Piece data I = 0, J = 1, L = 2, O = 3, S = 4, T = 5, Z = 6
         // Vector3Int position 
-        sensor.AddObservation(board.activePiece.position.x);
-        sensor.AddObservation(board.activePiece.position.y);
+        sensor.AddObservation(activePiece.position.x);
+        sensor.AddObservation(activePiece.position.y);
         // Int Tetromino
-        sensor.AddObservation((int)board.activePiece.data.tetromino);
+        sensor.AddObservation((int)activePiece.data.tetromino);
         // Int rotationIndex
-        sensor.AddObservation((int)board.activePiece.rotationIndex);
+        sensor.AddObservation((int)activePiece.rotationIndex);
+
+        //TODO: detect all tiles in tilemap, use forloop to scan through every tile with tilemap.HasTile(tilePosition) add 10x20(board bound xy) total of 200 tiles of observations, problem: we need to let the agent know what the active piece's tile is.
     }
 
     private void Start()
     {
         OnEpisodeBegin();
+
+    }
+    public override void WriteDiscreteActionMask(IDiscreteActionMask actionMask)
+    {
+        //actionMask.SetActionEnabled(0, 3, false);
     }
     public override void OnEpisodeBegin()
     {
         board = GetComponent<Board>();
+        activePiece = board.activePiece;
+        activePiece.AgentExists();
     }
     public override void OnActionReceived(ActionBuffers actions)
     {
 
         int PieceMove = actions.DiscreteActions[0]; // Get the action (1-5)
 
-       switch (PieceMove)
+        switch (PieceMove)
         {
-            case 0: board.activePiece.RotateAgent(1); break;
-            case 1: board.activePiece.RotateAgent(-1); break;
-            case 2: board.activePiece.MoveAgent(Vector2Int.left); break;
-            case 3: board.activePiece.MoveAgent(Vector2Int.down); break;
-            case 4: board.activePiece.MoveAgent(Vector2Int.right); break;
+            case 0: activePiece.HandleUpdateMove((int)MovementInput.left); break;
+            case 1: activePiece.HandleUpdateMove((int)MovementInput.right); break;
+            case 2: activePiece.HandleUpdateMove((int)MovementInput.softdrop); break;
+            case 3: activePiece.HandleUpdateMove((int)MovementInput.harddrop); break;
+            case 4: activePiece.HandleUpdateMove((int)MovementInput.rotateclockwise); break;
+            case 5: activePiece.HandleUpdateMove((int)MovementInput.rotatecounterclockwise); break;
             default: throw new ArgumentException("Invalid action value");
         }
- /**
+        /**
 switch (PieceMove)
-        {
-            case 0: Debug.Log("1"); break;
-            case 1: Debug.Log("2"); break;
-            case 2: Debug.Log("3"); break;
-            case 3: Debug.Log("4"); break;
-            case 4: Debug.Log("5"); break;
-            default: throw new ArgumentException("Invalid action value");
-        }
-        **/
+               {
+                   case 0: Debug.Log("1"); break;
+                   case 1: Debug.Log("2"); break;
+                   case 2: Debug.Log("3"); break;
+                   case 3: Debug.Log("4"); break;
+                   case 4: Debug.Log("5"); break;
+                   default: throw new ArgumentException("Invalid action value");
+               }
+               **/
     }
 }
