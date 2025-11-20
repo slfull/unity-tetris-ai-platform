5771d331118e048e2941eaf287c2a2c30952b8cb
slfull <83539187+slfull@users.noreply.github.com>
2025-11-20 18:53:40 +0800

    feat: change trainer type

--- Assets/Scripts/Agent/TetrisAgent.cs
+++ Assets/Scripts/Agent/TetrisAgent.cs
@@ -1,110 +1,75 @@
-using System.Collections;
 using System.Collections.Generic;
 using System;
 using UnityEngine;
-using System.Linq;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
-using Unity.Collections;
 using TMPro;
-using Mirror.Examples.MultipleAdditiveScenes;
-
+using Unity.MLAgents.Integrations.Match3;
 public class TetrisAgent : Agent
 {
-    public const int NUM_TETROMINO_TYPES = (int)Tetromino.Z + 1;
-    // 0 -> rotate 1 -> move 2 -> drop move 3 -> rotate lock
     private Board board;
-    private Queue<Movement> movements;
-    [Range(0.01f, 1f)] // Allows setting the timescale in the Inspector
-    public float timeSpeed = 1f;
-    private float nextTime;
-
-    [Header("Agent Reward")]
-    [SerializeField] private float actionReward;
-    [SerializeField] private float gameOverReward = -1f;
-    [Header("UI")]
-    [SerializeField] private TextMeshProUGUI epsodeUI;
-    [SerializeField] private TextMeshProUGUI rewardUI;
-    private int epsode;
+    private int ep;
     private float reward;
-    private int step;
+    private Queue<Movement> movements;
+    private const int NUM_TETROMINO_TYPES = 7;
+    private float gameOverReward = -1f;
+    private float lineReward = 0.1f;
+    [SerializeField] private TextMeshProUGUI epUI;
+    [SerializeField] private TextMeshProUGUI rewardUI;
 
-    void Update()
+    private void Start()
     {
-        if (Time.time >= nextTime)
-        {
-            nextTime = Time.time + timeSpeed;
-            AgentStep();
-        }
-        if (epsodeUI != null && rewardUI != null)
-        {
-            epsodeUI.text = "Epsode: " + epsode;
-            rewardUI.text = "Reward: " + reward;
-        }
-        
+        Init();
     }
 
-    public override void CollectObservations(VectorSensor sensor)
+    private void Update()
     {
-        sensor.AddOneHotObservation((int)board.activePiece.data.tetromino, NUM_TETROMINO_TYPES);
-        sensor.AddOneHotObservation((int)board.nextPiece.data.tetromino, NUM_TETROMINO_TYPES);
-        sensor.AddOneHotObservation((int)board.nextPiece2.data.tetromino, NUM_TETROMINO_TYPES);
-        sensor.AddOneHotObservation((int)board.nextPiece3.data.tetromino, NUM_TETROMINO_TYPES);
-        sensor.AddOneHotObservation((int)board.nextPiece4.data.tetromino, NUM_TETROMINO_TYPES);
-        sensor.AddOneHotObservation((int)board.nextPiece5.data.tetromino, NUM_TETROMINO_TYPES);
+        if(board != null)
+        {
+            AgentStep();            
+        }
+
+        if(epUI != null && rewardUI != null)
+        {
+            epUI.text = $"episode: {ep}";
+            rewardUI.text = $"reward: {reward}";
+        }
     }
-    public override void Initialize()
+
+    private void Init()
     {
         board = GetComponent<Board>();
         board.onGameOver += OnGameOver;
         board.onPieceLock += OnPieceLock;
         movements = new Queue<Movement>();
-        nextTime = Time.time;
-        actionReward = 1f / MaxStep;
-        epsode = 0;
-        reward = 0;
-
-        if(board != null && board.activePiece != null)
-        {
-            board.activePiece.AgentExists();
-        }
+        ep = 0;
+        reward = 0f;
     }
+
     public override void OnEpisodeBegin()
     {
-        reward = 0;
-        step = 0;
-        epsode++;
-        RequestDecision();
+        ep++;
+        reward = 0f;
     }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        AgentReward(RewardType.Action, 0);
-        step++;
-        int firstRotate = actions.DiscreteActions[0] - 1;
-        int firstMove = actions.DiscreteActions[1] - 5;
-        int secondMove = actions.DiscreteActions[2] - 5;
-        int secondRotate = actions.DiscreteActions[3] - 1;
-        bool firstDir = firstMove > 0;
-        bool secondDir = secondMove > 0;
-        firstMove = Math.Abs(firstMove);
-        secondMove = Math.Abs(secondMove);
-
-        //Debug.Log($"[ML Agent] First Move:{firstMove} First Rotate:{firstRotate} Second Move:{secondMove} Second Rotate:{secondRotate}");
-
-        RotateAgent(firstRotate);
-        MoveAgent(firstMove, firstDir);
-        movements.Enqueue(Movement.DROP);
-        MoveAgent(secondMove, secondDir);
-        RotateAgent(secondRotate);
+        int rotate = actions.DiscreteActions[0] - 1;
+        int move = actions.DiscreteActions[1] - 5;
+        
+        MoveAgent(move);
+        RotateAgent(rotate);
         movements.Enqueue(Movement.HARDDROP);
     }
-    private void MoveAgent(int step, bool dir)
+
+    private void MoveAgent(int step)
     {
-        for (int i = 0; i < step; i++)
+        bool dir = step > 0;
+
+        for(int i = 0; i < Math.Abs(step); i++)
         {
-            if (dir)
+            if(dir)
             {
                 movements.Enqueue(Movement.RIGHT);
             }
@@ -117,12 +82,12 @@ public class TetrisAgent : Agent
 
     private void RotateAgent(int step)
     {
-        if (step == -1)
+        if(step == -1)
         {
             movements.Enqueue(Movement.CCW);
         }
 
-        for (int i = 0; i < step; i++)
+        for(int i = 0; i < step; i++)
         {
             movements.Enqueue(Movement.CW);
         }
@@ -134,60 +99,74 @@ public class TetrisAgent : Agent
         {
             board.PieceMove(movements.Dequeue());
         }
-
-        if(step == MaxStep)
+        else
         {
-            board.GameReset();
+            RequestDecision();
         }
     }
-    public void OnGameOver()
+
+    public override void CollectObservations(VectorSensor sensor)
     {
-        AgentReward(RewardType.GameOver, 0);
-        EndEpisode();
+        sensor.AddOneHotObservation((int)board.activePiece.data.tetromino, NUM_TETROMINO_TYPES);
+        sensor.AddObservation(GetStates());
     }
 
-    public void OnPieceLock(int line, int combo, bool isRotation, bool b2b)
+    private float[] GetStates()
     {
-        int total = line;
-        if (isRotation)
+        int width = board.Bounds.width;
+        int height = board.Bounds.height;
+        float[] states = new float[width * height + width + 3];
+        bool[] fields = board.GetField(true);
+
+        for(int x = 0; x < width; x++)
         {
-            total += line;
-            if (b2b)
+            for(int y = 0; y < height; y++)
             {
-                total += line;
+                states[x + y * width] = fields[x + y * width]? 1f : 0f;
             }
         }
 
-        if (b2b && line == 4)
+        var (columnHeight, aggregateHeight, completedLines, holes, bumpiness) 
+            = Observations.GetCalculatedObservations(fields, width, height);
+        
+        for(int i = 0; i < width; i++)
         {
-            total += 2;
+            states[height * width + i] = (float)columnHeight[i] / height;
         }
 
-        total += Math.Min(combo, 4);
-        if (line > 0)
-        {
-            AgentReward(RewardType.LineClear, total);
-        }
-        RequestDecision();
+        states[height * width + width] = (float)aggregateHeight / (height * width);
+        states[height * width + width + 1] = (float)holes / (height * width);
+        states[height * width + width + 2] = (float)bumpiness / (height * width);
+
+        return states;
     }
 
-    private void AgentReward(RewardType type, int line)
+    public void OnGameOver()
     {
-        if (type == RewardType.Action)
+        AgentReward(RewardType.GameOver, 0);
+        //Debug.Log("GameOver" + ep);
+        EndEpisode();
+    }
+
+    public void OnPieceLock(int line, int combo, bool isLastMoveRotation, bool isB2B)
+    {
+        if(line > 0)
         {
-            float temp = 0.1f; // need fix
-            AddReward(temp * 0.01f);
-            reward += temp * 0.01f;
+            AgentReward(RewardType.LineClear, line);
         }
-        if (type == RewardType.LineClear)
-        {
-            AddReward(line * 0.760666f);
-            reward += line * 0.760666f;
-        }
+    }
+
+    public void AgentReward(RewardType type, int line)
+    {
         if(type == RewardType.GameOver)
         {
             AddReward(gameOverReward);
             reward += gameOverReward;
+        }
+        if(type == RewardType.LineClear)
+        {
+            AddReward(line * lineReward);
+            reward += line * lineReward;
         }
     }
 }
